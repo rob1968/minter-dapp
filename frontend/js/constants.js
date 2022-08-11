333e7589834faccac7ffd0a3c5877fde2c00e25f
Flop <robvandermeer@yahoo.com>
2022-08-11 06:09:22 +0200

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x4F6e1A15a26c2c79B4a76C88CbF287B5c38C886C"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
